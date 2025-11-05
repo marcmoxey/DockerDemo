734926c99ff99a58d0180caf81b3beaa202d5cae
Marc Moxey <moxeymarc@gmail.com>
2025-11-05 08:58:15 -0500

    Files upload

---
+++ SampleApp/SampleWebApp/Calculation.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace SampleWebApp
+{
+    public class Calculation
+    {
+
+        [Theory]
+        [InlineData(4,3,7)]
+        [InlineData(8,3,11)]
+        [InlineData(12,13,2.5)]
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ SampleApp/TestProj/CalculationTest.cs
@@ -0,0 +1,18 @@
+﻿using SampleWebApp;
+
+namespace TestProj
+{
+    public class CalculationTest
+    {
+        [Theory]
+        [InlineData(4,3,7)]
+        [InlineData(8, 3, 11)]
+        [InlineData(12, 13, 25)]
+        public void Add_ShouldWork(double x, double y, double  expected)
+        {
+            double actual = Calculation.Add(x,y);
+            
+            Assert.Equal(expected, actual);
+        }
+    }
+}
